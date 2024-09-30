685f6ae32ea3115adf0050b0531b7aa95a08ebaa
chanpark5660 <chanp5660@koast.net>
2024-09-30 16:37:02 +0900

    Add Blog Post

--- obsidian_to_blog_sync.bat
+++ obsidian_to_blog_sync.bat
@@ -1,4 +1,10 @@
 @echo off
 call C:\Users\user\anaconda3\Scripts\activate.bat chanp5660
 python obsidian_to_blog_sync.py
+
+REM Git 명령어 실행
+git add .
+git commit -m "Add Blog Post"
+git push origin
+
 pause
